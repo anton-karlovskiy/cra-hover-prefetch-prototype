0960289ec198bb455450f84344f9d61cd28bab94
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-02-07 00:53:04 +0300

    test: installed updated webpack-route-manifest from GitHub

--- src/hoc/PrefetchLink/index.js
+++ src/hoc/PrefetchLink/index.js
@@ -8,22 +8,24 @@ import './prefetch-link.css';
 
 const PrefetchLink = ({ children, to, ...rest }) => {
   const onMouseEnterHandler = async event => {
-    if (!window._rmanifest_) {
-      console.log('[hoc PrefetchLink] route manifest is not stored');
-      const response = await fetch('/rmanifest.json');
-      const data = await response.json();
-      // attach route manifest to global
-      window._rmanifest_ = data;
-    } else {
-      console.log('[hoc PrefetchLink] route manifest is already stored');
-    }
+    // ray test touch <
+    // if (!window._rmanifest_) {
+    //   console.log('[hoc PrefetchLink] route manifest is not stored');
+    //   const response = await fetch('/rmanifest.json');
+    //   const data = await response.json();
+    //   // attach route manifest to global
+    //   window._rmanifest_ = data;
+    // } else {
+    //   console.log('[hoc PrefetchLink] route manifest is already stored');
+    // }
     
-    const entry = rmanifest(window._rmanifest_, to);
-    const chunkURLs = entry.files.map(file => file.href);
-    console.log('[hoc PrefetchLink] to, chunkURLs => ', to, chunkURLs);
-    if (chunkURLs.length > 0) {
-      prefetch(chunkURLs);
-    }
+    // const entry = rmanifest(window._rmanifest_, to);
+    // const chunkURLs = entry.files.map(file => file.href);
+    // console.log('[hoc PrefetchLink] to, chunkURLs => ', to, chunkURLs);
+    // if (chunkURLs.length > 0) {
+    //   prefetch(chunkURLs);
+    // }
+    // ray test touch >
   };
 
   return (
